cc8e95c2d06515d3abde8887597e9ffb94dbd149
Seonny Kang <bangti77@hotmail.com>
2024-04-04 23:28:14 +0900

    functions finished

---
+++ 21.functions.py
@@ -0,0 +1,7 @@
+
+def hello(first_name,last_name,age):
+    print("hello "+first_name+" "+last_name)
+    print("You are "+str(age)+" years old")
+    print("Have a nice day!")
+
+hello("Bro","Code",21)
