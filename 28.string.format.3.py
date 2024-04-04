8993c0267acbb8f56a12af498032881f5e1c9606
Seonny Kang <bangti77@hotmail.com>
2024-04-05 00:18:21 +0900

    string format 3 finished

---
+++ 28.string.format.3.py
@@ -0,0 +1,10 @@
+
+number = 1000
+
+print("The number pi is {:.3f}".format(number))
+print("The number is {:,}".format(number))
+print("The number is {:b}".format(number))
+print("The number is {:o}".format(number))
+print("The number is {:x}".format(number))
+print("The number is {:E}".format(number))
+
