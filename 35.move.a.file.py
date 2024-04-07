9f69eecfc72bd9bd2a6976142924e13724e5a51f
Seonny Kang <bangti77@hotmail.com>
2024-04-07 21:35:01 +0900

    move a file finished

---
+++ 35.move.a.file.py
@@ -0,0 +1,14 @@
+import os
+
+source = "test35.txt"
+destination = "D:\\study\\python\\py_tutorial\\folder\\test35.txt"
+
+try:
+    if os.path.exists(destination):
+        print("There is already a file there")
+    else:
+        os.replace(source, destination)
+        print(source + " was moved")
+except FileNotFoundError:
+    print(source + " was not found")
+
