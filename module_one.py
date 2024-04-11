3fa49a5664c3a7e5a32f3592e9a2013a190a9ade
Seonny Kang <bangti77@hotmail.com>
2024-04-11 21:01:43 +0900

    if __name__ == __main__ finished

---
+++ module_one.py
@@ -0,0 +1,12 @@
+
+def hello():
+    print("Hello!")
+
+# if __name__ == '__main__':
+#     print("running this module directly")
+# else:
+#     print("running other module indirectly")
+
+if __name__ == '__main__':
+    hello()
+
