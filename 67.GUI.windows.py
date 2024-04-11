866748306b55d24dc43bdf7be359433c20008174
Seonny Kang <bangti77@hotmail.com>
2024-04-11 22:06:44 +0900

    GUI windows finished

---
+++ 67.GUI.windows.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("420x420")
+window.title("Bro Code first GUI program")
+
+icon = PhotoImage(file='logo.png')
+window.iconphoto(True,icon)
+window.config(background="#5cfcff")
+
+window.mainloop()
+
+
+
+
