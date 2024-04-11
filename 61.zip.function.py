ba90c9d23b486e39365729e9114fad63c3d1a538
Seonny Kang <bangti77@hotmail.com>
2024-04-11 20:49:52 +0900

    zip function finished

---
+++ 61.zip.function.py
@@ -0,0 +1,20 @@
+
+usernames = ["Dude", "Bro", "Mister"]
+passwords = ("p@ssword", "abc123", "guest")
+login_date = ["1/1/2021", "1/2/2021", "1/3/2021"]
+
+# users = list(zip(usernames, passwords))
+# print(type(users))
+# for i in users:
+#     print(i)
+
+# users = dict(zip(usernames, passwords))
+# print(type(users))
+# for key,value in users.items():
+#     print(key+" : "+value)
+
+users = zip(usernames, passwords, login_date)
+for i in users:
+    print(i)
+
+
